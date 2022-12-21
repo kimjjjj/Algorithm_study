89d875bc5d12b83f6303dfc75cf644958f6088bf
kimjjjj <102236761+kimjjjj@users.noreply.github.com>
2022-12-21 22:21:17 +0900

    Create Solution.java
    평균 구하기 소스

---
+++ kimjs/programmers/lv1/java/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double solution(int[] arr) {
+      int sumInt = 0;
+      double answer = 0;
+      
+      // 합 계산
+      for(int i=0; i<arr.length; i++) {
+        sumInt += arr[i];
+      }
+      
+      // 평균 계산
+      answer = (double) sumInt / arr.length;
+      
+      return answer;
+    }
+}
