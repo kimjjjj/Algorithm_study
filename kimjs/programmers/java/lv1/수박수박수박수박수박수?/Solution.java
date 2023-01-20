9da886ed0374beef7c606eb529bf73a57e93173e
kimjjjj <102236761+kimjjjj@users.noreply.github.com>
2023-01-20 20:13:29 +0900

    Create Solution.java
    수박수박수박수박수박수? 코드

---
+++ kimjs/programmers/java/lv1/수박수박수박수박수박수?/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        for (int i=0; i<n; i++) answer = (i&1) == 0 ? answer + "수" : answer + "박";
+        
+        return answer;
+    }
+}
