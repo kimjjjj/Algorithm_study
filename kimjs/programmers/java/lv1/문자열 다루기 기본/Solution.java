1a1dc981daa92994515d6651e04187d5a3da92a3
kimjjjj <102236761+kimjjjj@users.noreply.github.com>
2023-02-08 22:22:23 +0900

    Create Solution.java
    문자열 다루기 기본 코드

---
+++ kimjs/programmers/java/lv1/문자열 다루기 기본/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = false;
+        
+        if (s.length() == 4 || s.length() == 6) {
+            /********** code 1 **********/
+            answer = s.matches("[+-]?\\d*(\\.\\d+)?");
+            /****************************/
+            
+            /********** code 2 **********/
+            answer = s.matches("(^[0-9]*$)");
+            /****************************/
+        }
+        
+        return answer;
+    }
+}
