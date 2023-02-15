59e957384fe686aa906db6608a521b5e413e5489
kimjjjj <102236761+kimjjjj@users.noreply.github.com>
2023-02-16 06:03:21 +0900

    Create Solution.java
    이상한 문자 만들기 코드

---
+++ kimjs/programmers/java/lv1/이상한 문자 만들기/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split("");
+        int idx = 0;
+        
+        for (int i=0; i<arr.length; i++) {
+            
+            if (" ".equals(arr[i])) {
+                idx = 0;
+                
+            } else if (idx % 2 == 0){
+                arr[i] = arr[i].toUpperCase();
+                idx++;
+                
+            } else if (idx % 2 != 0) {
+                arr[i] = arr[i].toLowerCase();
+                idx++;
+                
+            }
+            
+            answer += arr[i];
+        }
+        
+        return answer;
+    }
+}
